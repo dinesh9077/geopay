042b9cd36dc5b28b0201089a73befe38ea78c845
dinesh9077 <94532938+dinesh9077@users.noreply.github.com>
2024-11-13 12:39:22 +0530

    ad

--- resources/views/user/kyc/metamapkyc.blade.php
+++ resources/views/user/kyc/metamapkyc.blade.php
@@ -76,7 +76,7 @@
 				// Set the necessary attributes for the button
 				metamapButton.setAttribute('clientid', metaClientId);
 				metamapButton.setAttribute('flowId', metaFlowId);
-				metamapButton.setAttribute('metadata', '{"user_id": "{{ $user->id }}"}');  // You can pass other metadata here
+				metamapButton.setAttribute('metadata', '{"user_id": "{{ $user->id }}","user": "{{ $user->first_name.' '.$user->last_name }}"}');  // You can pass other metadata here
 				
 				// Append the MetaMap button to the container
 				metamapButtonContainer.appendChild(metamapButton);
