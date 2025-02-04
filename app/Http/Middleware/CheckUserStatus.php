9427f93d30523b8bd84d99839ad0b9043699e95e
dinesh9077 <94532938+dinesh9077@users.noreply.github.com>
2025-02-04 17:02:49 +0530

    asd

--- app/Http/Middleware/CheckUserStatus.php
+++ app/Http/Middleware/CheckUserStatus.php
@@ -19,7 +19,7 @@ class CheckUserStatus
 		// Check if user is authenticated and status is 0
         if (Auth::check() && Auth::user()->status == 0) {
             Auth::logout(); // Logout the user
-            return redirect()->route('login')->with('error', 'Your account is inactive. Please contact support.');
+            return redirect()->route('login')->with('error', 'This user account is inactive. Please reach out to the administrator for further details.');
         }
 		
         return $next($request);
