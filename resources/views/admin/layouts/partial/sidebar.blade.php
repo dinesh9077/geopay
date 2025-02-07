4b3d3e9d34e81ae28567b4055d32fd2d94b4cc7e
dinesh9077 <94532938+dinesh9077@users.noreply.github.com>
2025-02-07 12:02:21 +0530

    asd

--- resources/views/admin/layouts/partial/sidebar.blade.php
+++ resources/views/admin/layouts/partial/sidebar.blade.php
@@ -173,28 +173,28 @@
             @endif
 			
 			@php
-                $Reports = [
+                $reports = [
                     'transaction_mobile_money_onafric' => ['route' => 'admin.transaction.mobile-money-onafric', 'label' => 'Oafric Mobile Money'],
                     'transaction_bank_onafric' => ['route' => 'admin.transaction.bank-onafric', 'label' => 'Oafric Transfer Bank'],
                     'transaction_bank_lightnet' => ['route' => 'admin.transaction.bank-lightnet', 'label' => 'Lightnet Transfer Bank'],
                 ];
             @endphp
 
-            @if (collect($Reports)->keys()->some(fn($key) => config("permission.$key.view")))
+            @if (collect($reports)->keys()->some(fn($key) => config("permission.$key.view")))
 
                 <li
-                    class="nav-item {{ collect($Reports)->contains(fn($item) => request()->routeIs($item['route'])) ? 'active' : '' }}">
+                    class="nav-item {{ collect($reports)->contains(fn($item) => request()->routeIs($item['route'])) ? 'active' : '' }}">
                     <a class="nav-link" data-bs-toggle="collapse" href="#reports" role="button"
-                        aria-expanded="{{ collect($Reports)->contains(fn($item) => request()->routeIs($item['route'])) ? 'true' : 'false' }}"
+                        aria-expanded="{{ collect($reports)->contains(fn($item) => request()->routeIs($item['route'])) ? 'true' : 'false' }}"
                         aria-controls="reports">
                         <i class="link-icon" data-feather="file"></i>
                         <span class="link-title">All Transaction</span>
                         <i class="link-arrow" data-feather="chevron-down"></i>
                     </a>
-                    <div class="collapse {{ collect($Reports)->contains(fn($item) => request()->routeIs($item['route'])) ? 'show' : '' }}"
+                    <div class="collapse {{ collect($reports)->contains(fn($item) => request()->routeIs($item['route'])) ? 'show' : '' }}"
                         id="reports">
                         <ul class="nav sub-menu">
-                            @foreach ($Reports as $key => $item)
+                            @foreach ($reports as $key => $item)
                                 @if (config("permission.$key.view"))
                                     <li class="nav-item">
                                         <a href="{{ route($item['route']) }}"
