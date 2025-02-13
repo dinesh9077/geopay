138a53e5fa7f6efee401741aad3b9e7ec685f5fb
dinesh9077 <94532938+dinesh9077@users.noreply.github.com>
2025-02-13 14:12:55 +0530

    asd

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -60,7 +60,7 @@
 				
 				$token = $user->createToken('geopay')->accessToken;
 				$user->load('companyDetail'); 
-				$user->profile_image = url('storage/profile', $user->profile_image);
+				$user->profile_image = $user->profile_image ? url('storage/profile', $user->profile_image) : url('admin/default-profile.png');
 				$user->token = $token;
 
 				Helper::loginLog('login', $user, 'App'); 
@@ -96,7 +96,7 @@
 				$token = $request->bearerToken();  
 				
 				$user = Auth::user(); 
-				$user->profile_image = url('storage/profile', $user->profile_image);
+				$user->profile_image = $user->profile_image ? url('storage/profile', $user->profile_image) : url('admin/default-profile.png');
 				$user->load('companyDetail'); 
 				$user->token = $token;
  
